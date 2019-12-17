48a19d08a2ea321b0d91cc0218de6df40536949f
ArundaVictor <victoroyugi31@gmail.com>
2019-12-17 11:43:54 +0300

    updates

---
+++ src/main/java/com/gymapp/service/portal/SecurityConfig.java
@@ -0,0 +1,4 @@
+package com.gymapp.service.portal;
+
+public class SecurityConfig {
+}
--- src/main/java/com/gymapp/service/portal/controller/UserController.java
+++ src/main/java/com/gymapp/service/portal/controller/UserController.java
@@ -20,7 +20,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("user-controller")
-@CrossOrigin(origins = "*", maxAge = 3600)
+@CrossOrigin(origins = { "http://localhost:4200" }, maxAge = 3600)
 public class UserController {
 
     @Autowired
