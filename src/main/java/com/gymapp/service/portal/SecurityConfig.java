edc011d7eabebb51a3e3f65be7a82885be7e4f0a
ArundaVictor <victoroyugi31@gmail.com>
2020-01-07 12:54:08 +0300

    added cors

--- src/main/java/com/gymapp/service/portal/SecurityConfig.java
+++ src/main/java/com/gymapp/service/portal/SecurityConfig.java
@@ -1,7 +1,18 @@
 package com.gymapp.service.portal;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-public class SecurityConfig {
+@Configuration
+@EnableWebMvc
+public class SecurityConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/rest/v2/**").allowedMethods("PUT", "DELETE", "GET", "POST");
+    }
 
 
 }
